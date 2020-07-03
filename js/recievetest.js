32ad83bf1e56cdf0442d2d203112b58f0b222052
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2020-07-03 23:24:42 +0900

    tess

--- js/recievetest.js
+++ js/recievetest.js
@@ -2,7 +2,7 @@ const api = document.getElementById('apiKey').value;
 const connection = document.getElementById('createconnection');
 connection.onclick = function()
 {
-    const peer = createPeer(api.textContent);
+    const peer = createPeer(api.value);
     //createDataConnection(peer);
     //dataconnection = createDataConnection(peer);
 };
